e448fe2994c1451a496758190771dd3ef59132ea
loading <49134864+load1n9@users.noreply.github.com>
2021-12-22 06:37:11 -0800

    feat: relu6 operator

--- backend/webgpu/operators/unary.ts
+++ backend/webgpu/operators/unary.ts
@@ -29,6 +29,7 @@ export const abs = unary("return abs(a);");
 export const linear = unary("return a;");
 export const neg = unary("return -a;");
 export const relu = unary<"f32" | "i32">((type) => `return max(a, 0${type});`);
+export const relu6 = unary<"f32" | "i32">((type) => `return clamp(a, 0${type}, 6.0);`);
 export const ceil = unary<"f32">("return ceil(a);");
 export const floor = unary<"f32">("return floor(a);");
 export const round = unary<"f32">("return round(a);");
@@ -38,30 +39,30 @@ export const sigmoid = unary<"f32">("return 1.0 / (1.0 + exp(-1.0 * a));");
 export const square = unary<"f32">("return a * a;");
 export const cos = unary<"f32">("return cos(a);");
 export const cosh = unary<"f32">(`
-  let e2x = exp(-a);
-  return (e2x + 1.0 / e2x) / 2.0;
+let e2x = exp(-a);
+return (e2x + 1.0 / e2x) / 2.0;
 `);
 export const sin = unary<"f32">("return sin(a);");
 export const sinh = unary<"f32">(`
-  let e2x = exp(a);
-  return (e2x - 1.0 / e2x) / 2.0;
+let e2x = exp(a);
+return (e2x - 1.0 / e2x) / 2.0;
 `);
 export const tan = unary<"f32">("return tan(a);");
 export const tanh = unary<"f32">(`
-  let e2x = exp(-2.0 * abs(a));
-  return sign(a) * (1.0 - e2x) / (1.0 + e2x);
+let e2x = exp(-2.0 * abs(a));
+return sign(a) * (1.0 - e2x) / (1.0 + e2x);
 `);
 export const exp = unary<"f32">("return exp(a);");
 export const elu = unary<"f32">(`
-  if (a >= 0.0) {
-    return a;
-  } 
-  return (exp(a) - 1.0);
+if (a >= 0.0) {
+  return a;
+} 
+return (exp(a) - 1.0);
 `);
-export const log = unary<"f32">(`
-  if (a < 0.0) {
-    return 1.0 / 0.0;
-  }
-  return log(a);
+export const log = unary<"f32">((type) => `
+if (a < 0.0) { 
+  return 1.0/0${type}; 
+} 
+return log(a);
 `);
 // export const leakyrelu = unary<"f32" | "i32">((type) => `if (a < 0${type}) { return uniforms.alpha * a; } return a;`);
