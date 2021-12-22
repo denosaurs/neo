324ec90f1f9992f2259716cd10772eab22a0f9ea
eliassjogreen <eliassjogreen1@gmail.com>
2021-12-22 18:07:04 +0100

    feat: inc, dec and relu6 unary ops
    Co-authored-by: Jin Wei Tan <CarrotzRule123@users.noreply.github.com>
    Co-authored-by: loading <load1n9@users.noreply.github.com>

--- backend/webgpu/operators/unary.ts
+++ backend/webgpu/operators/unary.ts
@@ -28,7 +28,10 @@ export function unary<T extends DataType>(
 export const abs = unary("return abs(a);");
 export const linear = unary("return a;");
 export const neg = unary("return -a;");
+export const inc = unary("return a = a + 1;");
+export const dec = unary("return a = a - 1;");
 export const relu = unary<"f32" | "i32">((type) => `return max(a, 0${type});`);
+export const relu6 = unary<"f32" | "i32">((type) => `return clamp(a, 0${type}, 6${type});`);
 export const ceil = unary<"f32">("return ceil(a);");
 export const floor = unary<"f32">("return floor(a);");
 export const round = unary<"f32">("return round(a);");
